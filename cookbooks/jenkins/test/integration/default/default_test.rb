c49e03ffee102ae1b1b6ce3f996dffc7016182e1
Priyesh Srivastava <priyesh007srivastava@gmail.com>
2021-03-10 18:40:46 +0000

    test

--- cookbooks/jenkins/test/integration/default/default_test.rb
+++ cookbooks/jenkins/test/integration/default/default_test.rb
@@ -3,14 +3,21 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
 
-unless os.windows?
+#unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
+ # describe user('root'), :skip do
+  #  it { should exist }
+ # end
+#end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
-end
+#describe port(80), :skip do
+#  it { should_not be_listening }
+#end
+describe file('/root/.jenkins/secrets/initialAdminPassword') do
+ it { should exist } 
+end 
+
+describe command('curl localhost:8080') do 
+ its('stdout') { should match(/hudson/) } 
+end 
